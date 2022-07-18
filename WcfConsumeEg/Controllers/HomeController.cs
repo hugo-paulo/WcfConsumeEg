babc500d035cbc295b31d7cbebf42cac3b287c82
hugoe <hugoe@LAPTOP-4EREB8TH>
2022-07-18 10:22:15 +0100

    Add project files.

---
+++ WcfConsumeEg/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WcfConsumeEg.Models;
+
+namespace WcfConsumeEg.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Message()
+        {
+            int number = 47;
+            
+            ServiceReference1.Service1Client _client = new ServiceReference1.Service1Client("basicHttpBinding_IService");
+            var message = _client.GetData(number);
+
+            var display = new OutMessage { Message = message };
+
+            return View(display);
+        }
+
+    }
+}
